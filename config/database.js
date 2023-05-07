50b334ac1058e5e3857b43ec5a21c9997400df0c
your_github_DannyGarciaDEV <dannygarciadev@gmail.com>
2023-05-06 21:18:17 -0400

    fixing

--- config/database.js
+++ config/database.js
@@ -2,5 +2,5 @@
 module.exports = {
 
     'url' : 'mongodb+srv://dannygarciadev:rUfN6tMzFujP3x@cluster0.nbqmops.mongodb.net/test?retryWrites=true&w=majority', 
-    'dbName': 'test'
+    'dbName': 'webCam'
 };
